ad42c18fac2f14c3cac3f283e7232bc4853639e9
Dylan <dylanbradramsey@gmail.com>
2023-01-01 00:16:22 -0500

    Current progress

--- front-page.php
+++ front-page.php
@@ -5,14 +5,42 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>East Coast K9</title>
-
+  <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
   <?php
   wp_head();
   ?>
 </head>
 <body>
-  <h1>
-  This is my theme
-  </h1>
+  <section class="wrapper">
+    <nav class="site-header">
+      <div class="site-header__wrapper">
+        <img class="site-header__logo" src="/wp-content/uploads/2022/12/ECK-logo.webp" alt="">
+        <div class="site-header__locations-hours">
+          <p>
+            Located in North Port, Florida
+            <br>Serving Fort Myers to Bradenton
+          </p>
+          <p>
+            Mon-Wed, Fri 6:30am - 6pm
+            <br>Thursday 6:30am - 4pm
+          </p>          
+        </div>
+        <div>
+          <div class="site-header__email">
+            EastCoastK9LLC@gmail.com
+          </div>
+          <div class="site-header__phone">
+            941-374-7161
+          </div>
+        </div>
+      </div>
+    </nav>
+    <div class="site-banner">
+      <img src="/wp-content/uploads/2022/12/banner-scaled.webp" alt="">
+    </div>
+    <main class="site-body">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi nostrum qui odit veritatis, hic consequatur vel, ipsam natus ratione dolore possimus enim distinctio aut perspiciatis culpa odio voluptate sapiente accusantium!
+    </main>
+  </section>
 </body>
 </html>
