ffe47ca802ca8db65715cdba14db462c9698e521
Dylan <dylanbradramsey@gmail.com>
2023-01-02 09:58:55 -0500

    Current progress

--- front-page.php
+++ front-page.php
@@ -36,10 +36,10 @@
       </div>
     </nav>
     <div class="site-banner">
-      <img src="/wp-content/uploads/2022/12/banner-scaled.webp" alt="">
+      <img src="/wp-content/uploads/2022/12/banner-scaled.webp" alt="site banner">
     </div>
     <main class="site-body">
-      <section>
+      <section class="service-cards">
         <h1 class="heading heading--brown heading--center">East Coast K9</h1>
         <div class="service-cards-group">
           <div class="single-service-card">
@@ -54,6 +54,23 @@
             <img class="single-service-card--image" src="/wp-content/uploads/2023/01/spa-green.png" alt="">
             <h6>Spa</h6>
           </div>                                      
+        </div>
+      </section>
+      <section class="introduction">
+        <img class="introduction__image" src="/wp-content/uploads/gab-and-zarius.webp" alt="">
+        <div class="introduction__copy">
+          <h2 class="heading heading--brown">Who We Are...</h2>
+          <p>
+            East Coast K9 is dedicated to providing exceptional care for your canine companion in 
+            a family atmosphere. Our kennel room is newly renovated to ensure clean and 
+            comfortable lodging. Your pets will be happy and secure while staying with us, in our 
+            home. We believe that the key to your dog's success and happiness is a loving, low 
+            stress environment. Your dog will receive one-on-one attention as he or she gets 
+            acquainted with us and enjoys play time with other K9 friends. As a certified vet 
+            assistant and seasoned dog trainer, I can assure and promise the safest care for your 
+            pups! We strive to be their second home while away from home.            
+          </p>
+          <a class="button--green" href="">Sign up now</a>
         </div>
       </section>
     </main>
