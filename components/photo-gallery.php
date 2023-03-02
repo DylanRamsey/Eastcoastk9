6fc7a6b9cc82f008a30568985b3acd1a2383ee5c
Dylan <dylanbradramsey@gmail.com>
2023-03-02 01:40:37 -0500

    start of photo gallery

---
+++ components/photo-gallery.php
@@ -0,0 +1,34 @@
+<section class="photo-gallery">
+  <div class="photo-gallery__group">
+    <img class="photo-gallery__photo photo-gallery__photo--first" src="/wp-content/uploads/corgi-getting-a-bath.webp" alt="">
+    <img class="photo-gallery__photo photo-gallery__photo--second" src="/wp-content/uploads/dogs-sitting-for-their-handlers.webp" alt="">
+    <img class="photo-gallery__photo photo-gallery__photo--third" src="/wp-content/uploads/dog-laying-in-suitcase.webp" alt="">
+    <img class="photo-gallery__photo photo-gallery__photo--forth" src="/wp-content/uploads/homepage-training.webp" alt="">
+    <img class="photo-gallery__photo photo-gallery__photo--fifth" src="/wp-content/uploads/gab-and-zarius.webp" alt="">
+    <img class="photo-gallery__photo photo-gallery__photo--sixth" src="/wp-content/uploads/spa-pup.jpeg" alt="">
+  </div>
+  <div class="photo-gallery__group">
+    <img class="photo-gallery__photo photo-gallery__photo--first" src="/wp-content/uploads/corgi-getting-a-bath.webp" alt="">
+    <img class="photo-gallery__photo photo-gallery__photo--second" src="/wp-content/uploads/dogs-sitting-for-their-handlers.webp" alt="">
+    <img class="photo-gallery__photo photo-gallery__photo--third" src="/wp-content/uploads/dog-laying-in-suitcase.webp" alt="">
+    <img class="photo-gallery__photo photo-gallery__photo--forth" src="/wp-content/uploads/homepage-training.webp" alt="">
+    <img class="photo-gallery__photo photo-gallery__photo--fifth" src="/wp-content/uploads/gab-and-zarius.webp" alt="">
+    <img class="photo-gallery__photo photo-gallery__photo--sixth" src="/wp-content/uploads/spa-pup.jpeg" alt="">
+  </div>
+  <div class="photo-gallery__group">
+    <img class="photo-gallery__photo photo-gallery__photo--first" src="/wp-content/uploads/corgi-getting-a-bath.webp" alt="">
+    <img class="photo-gallery__photo photo-gallery__photo--second" src="/wp-content/uploads/dogs-sitting-for-their-handlers.webp" alt="">
+    <img class="photo-gallery__photo photo-gallery__photo--third" src="/wp-content/uploads/dog-laying-in-suitcase.webp" alt="">
+    <img class="photo-gallery__photo photo-gallery__photo--forth" src="/wp-content/uploads/homepage-training.webp" alt="">
+    <img class="photo-gallery__photo photo-gallery__photo--fifth" src="/wp-content/uploads/gab-and-zarius.webp" alt="">
+    <img class="photo-gallery__photo photo-gallery__photo--sixth" src="/wp-content/uploads/spa-pup.jpeg" alt="">
+  </div>
+  <div class="photo-gallery__group">
+    <img class="photo-gallery__photo photo-gallery__photo--first" src="/wp-content/uploads/corgi-getting-a-bath.webp" alt="">
+    <img class="photo-gallery__photo photo-gallery__photo--second" src="/wp-content/uploads/dogs-sitting-for-their-handlers.webp" alt="">
+    <img class="photo-gallery__photo photo-gallery__photo--third" src="/wp-content/uploads/dog-laying-in-suitcase.webp" alt="">
+    <img class="photo-gallery__photo photo-gallery__photo--forth" src="/wp-content/uploads/homepage-training.webp" alt="">
+    <img class="photo-gallery__photo photo-gallery__photo--fifth" src="/wp-content/uploads/gab-and-zarius.webp" alt="">
+    <img class="photo-gallery__photo photo-gallery__photo--sixth" src="/wp-content/uploads/spa-pup.jpeg" alt="">
+  </div>
+</section>
--- templates/gallery.php
+++ templates/gallery.php
@@ -1,6 +1,10 @@
 <?php /* Template Name: gallery */ ?>
 <?php get_header(); ?>
 
-gallery Page
+<?php get_header(); ?>
+<div class="site-body">
+  <?php include dirname(dirname(__FILE__))."/components/interior-heading-group.php" ?>
+  <?php include dirname(dirname(__FILE__))."/components/photo-gallery.php" ?>
+</div>
 
 <?php get_footer(); ?>
