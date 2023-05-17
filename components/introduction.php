0a3f6c684a1e26a6178d5dc720e54dcd663d0f06
Dylan <dylanbradramsey@gmail.com>
2023-05-16 23:33:48 -0400

    Refactored Introduction component to use the Flexible Content field within the Content Designer

--- components/introduction.php
+++ components/introduction.php
@@ -1,56 +1,31 @@
 <section class="introduction">
-  <img 
-    class="introduction__image" 
-    src="
-      <?php
-        $home_introduction_image = get_field("home_introduction_image");
-        if($home_introduction_image) {
-          echo $home_introduction_image;
-        }
-      ?>
-    " 
-    alt="
-      <?php
-        $home_introduction_alt = get_field("home_introduction_alt");
-        if($home_introduction_alt) {
-          echo $home_introduction_alt;
-        }
-      ?>          
-    "
-  >
-  <div class="introduction__copy">
-    <h2 class="heading heading--brown">
-      <?php
-        $home_introduction_heading = get_field("home_introduction_heading");
-        if($home_introduction_heading) {
-          echo $home_introduction_heading;
-        }
-      ?>                
-    </h2>
-    <p>
-      <?php
-        $home_introduction_text = get_field("home_introduction_text");
-        if($home_introduction_text) {
-          echo $home_introduction_text;
-        }
-      ?>      
-    </p>
-    <a 
-      class="button button--green" 
-      href="
-        <?php
-          $home_introduction_link = get_field("home_introduction_link");
-          if($home_introduction_link) {
-            echo $home_introduction_link;
-          }
-        ?>               
-      ">
-      <?php
-        $home_introduction_cta = get_field("home_introduction_cta");
-        if($home_introduction_cta) {
-          echo $home_introduction_cta;
-        }
-      ?>
-    </a>
-  </div>
+  <?php if( have_rows('content_designer') ): ?>
+    <?php while( have_rows('content_designer') ): the_row(); ?>
+      <?php if (get_row_layout() == 'introduction'):
+        $introduction_heading = get_sub_field('introduction_heading');
+        $introduction_image = get_sub_field('introduction_image');
+        $introduction_image_alt = get_sub_field('introduction_image_alt');
+        $introduction_button_text = get_sub_field('introduction_button_text');
+        $introduction_button_link = get_sub_field('introduction_button_link');
+        $introduction_text = get_sub_field('introduction_text');
+      ?>     
+    <img 
+      class="introduction__image" 
+      src="<?php echo $introduction_image;?>" 
+      alt="<?php echo $introduction_image_alt;?>" 
+    >
+    <div class="introduction__copy">
+      <h2 class="heading heading--brown">
+        <?php echo $introduction_heading;?>
+      </h2>
+      <p>
+        <?php echo $introduction_text;?>     
+      </p>
+      <a class="button button--green" href="<?php echo $introduction_button_link;?>">
+        <?php echo $introduction_button_text;?>
+      </a>
+    </div>
+    <?php endif;?>
+    <?php endwhile;?>
+  <?php endif;?>
 </section>
