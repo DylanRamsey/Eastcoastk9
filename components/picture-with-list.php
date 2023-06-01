e87a4edcfb82f22eaed39048db1cef8fefb459b5
Dylan <dylanbradramsey@gmail.com>
2023-06-01 19:19:43 -0400

    Refactored Image with list component to work with the content designer

--- components/picture-with-list.php
+++ components/picture-with-list.php
@@ -1,75 +1,48 @@
 <section class="picture-with-list">
-  <img 
-    class="picture-with-list__image" 
-    src="
-      <?php
-        $picture_with_list_image = get_field("picture_with_list_image");
-        if($picture_with_list_image) {
-          echo $picture_with_list_image;
-        }
+<?php if( have_rows('content_designer') ): ?>
+    <?php while( have_rows('content_designer') ): the_row(); ?>
+      <?php if (get_row_layout() == 'picture_with_list'):
+        $flip_component = get_sub_field('flip_component');
+        $picture_with_list_image = get_sub_field('picture_with_list_image');
+        $picture_with_list_image_alt = get_sub_field('picture_with_list_image_alt');
+        $picture_with_list_heading = get_sub_field('picture_with_list_heading');
+        $picture_with_list_button_text = get_sub_field('picture_with_list_button_text');
+        $picture_with_list_button_link = get_sub_field('picture_with_list_button_link');
+        $picture_with_list_items  = get_sub_field('picture_with_list_items');
       ?>
-    " 
-    alt="
-      <?php
-        $picture_with_list_image_alt = get_field("picture_with_list_image_alt");
-        if($picture_with_list_image_alt) {
-          echo $picture_with_list_image_alt;
-        }
-      ?>    
-    "
-  >
-  <div class="picture-with-list__content">
-    <h3 class="heading heading--brown">
-      <?php
-        $picture_with_list_heading = get_field("picture_with_list_heading");
-        if($picture_with_list_heading) {
-          echo $picture_with_list_heading;
-        }
+      <?php if ($flip_component == 'Normal') {
+        $flip_component_img_class = '';
+        $flip_component_content_class = '';
+      }
+      ?>
+      <?php if ($flip_component == 'Flipped') {
+        $flip_component_img_class = 'picture-with-list__image--flipped';
+        $flip_component_content_class = 'picture-with-list__content--flipped';
+      } 
       ?>      
-    </h3>
-    <ul class="paw-print-list">
-      <?php
-        $picture_with_list_image_list_item_1 = get_field("picture_with_list_image_list_item_1");
-        if($picture_with_list_image_list_item_1) {
-          echo '<li>' . $picture_with_list_image_list_item_1 . '</li>';
-        }
-      ?>
-      <?php
-        $picture_with_list_image_list_item_2 = get_field("picture_with_list_image_list_item_2");
-        if($picture_with_list_image_list_item_2) {
-          echo '<li>' . $picture_with_list_image_list_item_2 . '</li>';
-        }
-      ?>
-      <?php
-        $picture_with_list_image_list_item_3 = get_field("picture_with_list_image_list_item_3");
-        if($picture_with_list_image_list_item_3) {
-          echo '<li>' . $picture_with_list_image_list_item_3 . '</li>';
-        }
-      ?>
-      <?php
-        $picture_with_list_image_list_item_4 = get_field("picture_with_list_image_list_item_4");
-        if($picture_with_list_image_list_item_4) {
-          echo '<li>' . $picture_with_list_image_list_item_4 . '</li>';
-        }
-      ?>               
-    </ul>
-
-    <a 
-      class="button button--green" 
-      href="
-      <?php
-        $picture_with_list_image_link = get_field("picture_with_list_image_link");
-        if($picture_with_list_image_link) {
-          echo $picture_with_list_image_link;
-        }
-      ?>      
-      ">
-      <?php
-        $picture_with_list_button_text = get_field("picture_with_list_button_text");
-        if($picture_with_list_button_text) {
-          echo $picture_with_list_button_text;
-        }
-      ?>      
-    </a>    
-  </div>
+        <img 
+          class="picture-with-list__image <?php echo $flip_component_img_class;?>" 
+          src="<?php echo $picture_with_list_image;?>" 
+          alt="<?php echo $picture_with_list_image_alt;?>"
+        >
+        <div class="picture-with-list__content <?php echo $flip_component_content_class;?>">
+          <h3 class="heading heading--brown">
+            <?php echo $picture_with_list_heading;?>   
+          </h3>
+          <ul class="paw-print-list">  
+          <?php foreach($picture_with_list_items as $picture_with_list_item):?>
+              <li>
+                <?php echo $picture_with_list_item['picture_with_list_single_item'];?>
+              </li>
+          <?php endforeach;?>               
+          </ul>
+          <a 
+            class="button button--green" 
+            href="<?php echo $picture_with_list_button_link;?>">
+            <?php echo $picture_with_list_button_text;?>
+          </a>    
+        </div>
+       <?php endif;?>
+    <?php endwhile;?>
+  <?php endif;?>     
 </section>
