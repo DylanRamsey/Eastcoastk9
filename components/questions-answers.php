6e1b4e314ce36635a7877994ac3d0655fbe64ac0
Dylan <dylanbradramsey@gmail.com>
2023-06-23 00:33:14 -0400

    Wrapping up Q&A component for Content Designer

--- components/questions-answers.php
+++ components/questions-answers.php
@@ -1,34 +1,36 @@
 <section class="qs-and-as site-body">
 <?php
     $question_categories = get_sub_field('question_categories');
+    $questionsAndAnswersGroup = get_sub_field('q&a_group');
   ?>    
   <div class="qs-and-as__categories-group">
     <?php foreach($question_categories as $question_category): ?>
-      <a href="<?php echo $question_category['category_link'];?>">
         <h6 class="qs-and-as__category">
-          <?php echo $question_category['category_label'];?>   
+          <a href="<?php echo $question_category['category_link'];?>">
+            <?php echo $question_category['category_label'];?>   
+          </a>
         </h6>
-      </a>
     <?php endforeach;?>   
   </div>
-
-  <div class="qs-and-as__group">
-    <h4 class="qs-and-as__heading">Training</h4>
-    <div class="accordion">
-      <div class="accordion__question">
-        What kinds of training services do you offer?
-      </div>
-      <div class="accordion__answer">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      </div>
+  
+  <?php foreach($questionsAndAnswersGroup as $singleQAndAGroup): ?>
+    <div class="qs-and-as__group">
+      <h4 
+        class="qs-and-as__heading"
+        id="<?php echo $singleQAndAGroup['q&a_group_anchor'];?>"
+      >
+        <?php echo $singleQAndAGroup['q&a_group_heading'];?>
+      </h4>
+      <?php foreach($singleQAndAGroup['q&as'] as $qnaAccordion): ?>
+        <div class="accordion">
+          <div class="accordion__question">
+            <?php echo $qnaAccordion['question'];?>
+          </div>
+          <div class="accordion__answer">
+            <?php echo $qnaAccordion['answer'];?>
+          </div>
+        </div>
+      <?php endforeach;?>   
     </div>
-    <div class="accordion">
-      <div class="accordion__question">
-        What kinds of training services do you offer?
-      </div>
-      <div class="accordion__answer">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      </div>
-    </div>        
-  </div>
+  <?php endforeach;?>   
 </section>
