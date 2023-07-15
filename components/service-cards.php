5473b6949ab4a01c183ea7a02ae5dae0b1a211f9
Dylan <dylanbradramsey@gmail.com>
2023-07-15 01:57:05 -0400

    Updating service cards

--- components/service-cards.php
+++ components/service-cards.php
@@ -1,8 +1,11 @@
 <?php
+  $serviceCardHeading = get_sub_field('service_card_heading');
   $serviceCards = get_sub_field( 'service_card' );
 ?>
 <section class="site-body service-cards">
-  <h1 class="heading heading--brown heading--center">East Coast K9</h1>
+  <h1 class="heading heading--brown heading--center">
+    <?php echo $serviceCardHeading ?>
+  </h1>
   <div class="service-cards-group">
     <?php foreach($serviceCards as $serviceCard):?>
       <a 
@@ -10,11 +13,13 @@
         class="single-service-card"
       >
         <img 
-          class="single-service-card--image" 
+          class="single-service-card__image" 
           src="<?php echo $serviceCard['service_card_icon'];?>" 
           alt="<?php echo $serviceCard['service_card_icon_alt'];?>"
         />
-        <h6><?php echo $serviceCard['service_card_heading'];?></h6>
+        <h6 class="single-service-card__heading">
+          <?php echo $serviceCard['service_card_heading'];?>
+        </h6>
       </a>
     <?php endforeach;?>
   </div>
