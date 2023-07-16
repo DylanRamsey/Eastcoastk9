77a217c48adb5d88c4cea3574eb6fbfb4ce13aae
Dylan <dylanbradramsey@gmail.com>
2023-07-16 00:16:23 -0400

    Style fixes here and there, more fixes for service cards

--- components/service-cards.php
+++ components/service-cards.php
@@ -1,12 +1,28 @@
 <?php
   $serviceCardHeading = get_sub_field('service_card_heading');
-  $serviceCards = get_sub_field( 'service_card' );
+  $serviceCards = get_sub_field('service_card');
+  $serviceCardLayout = get_sub_field('service_card_layout');
+
 ?>
+
+<?php if($serviceCardLayout == '2 Column') {
+    $serviceCardLayoutClass = 'service-cards-group--two-col';
+  }
+?>
+<?php if($serviceCardLayout == '3 Column') {
+    $serviceCardLayoutClass = 'service-cards-group--three-col';
+  }
+?>
+<?php if($serviceCardLayout == '5 Column') {
+    $serviceCardLayoutClass = 'service-cards-group--five-col';
+  }
+?>
+
 <section class="site-body service-cards">
   <h1 class="heading heading--brown heading--center">
     <?php echo $serviceCardHeading ?>
   </h1>
-  <div class="service-cards-group">
+  <div class="service-cards-group <?= $serviceCardLayoutClass ?>">
     <?php foreach($serviceCards as $serviceCard):?>
       <a 
         href="<?php echo $serviceCard['service_card_link'];?>" 
